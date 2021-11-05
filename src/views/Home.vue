2ae0af69922a78e36f56144da2e48465750b7efd
li <1223757604@qq.com>
2021-11-05 17:56:04 +0800

    顶部 轮播图的上传

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,22 @@
 <template>
     <div>
-        
+        <header>
+            <Header />
+        </header>
+        <main>
+            
+        </main>
+        <footer>
+
+        </footer>
     </div>
 </template>
 <script>
-
+import Header from '@/views/header/header.vue'
 export default {
-
+    components:{
+        Header
+    }
 }
 </script>
 
