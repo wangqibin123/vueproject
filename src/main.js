1eb3bc5558d219aa9d4b1e35695bcaaf5edfda32
王棋彬 <mrwangqibin@163.com>
2021-11-05 10:52:55 +0800

    空项目

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+// import Vue from "vue/dist/vue"
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+// 移动端适配文件
+import "./assets/app"
+// 引入样式重置文件
+import "./assets/reset.css"
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+  // template:'<h1>1214</h1>'
+}).$mount('#app')
