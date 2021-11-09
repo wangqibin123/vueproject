e37d43d3dd4af022b591a67eee58e64db03de421
王棋彬 <mrwangqibin@163.com>
2021-11-09 08:05:55 +0800

    删除了element

--- src/main.js
+++ src/main.js
@@ -7,16 +7,11 @@ import store from './store'
 import "./assets/app"
 // 引入样式重置文件
 import "./assets/reset.css"
-import ElementUI from 'element-ui';
-// import 'element-ui/lib/theme-chalk/index.css';
 
-// import "./assets/element.css"
 
-Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 import "./assets/ant.css"
-// import 'ant-design-vue/dist/antd.css';
 import Antd from 'ant-design-vue'
 Vue.use(Antd);
 
