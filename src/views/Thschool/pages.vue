3dceba138d142b1293387796c9f01335e29e45b1
ring <2522564501@qq.com>
2021-11-15 19:21:28 +0800

    提交了三校生和艺考生

---
+++ src/views/Thschool/pages.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="page">
+    <p>
+      <router-link to="/" tag="span">当前位置：首页 ></router-link>
+      <router-link to="/hangkong" tag="span" class="hang">航空学院 ></router-link>
+      <router-link to="/sanxiaosheng" tag="span" class="hang">艺考生</router-link>
+    </p>
+  </div>
+</template>
+<style lang="scss" scoped>
+.page {
+  width: 100%;
+  margin: auto;
+  padding: .15rem 0;
+  color: #888888;
+  font-size: 0.14rem;
+  box-sizing: border-box;
+  p{
+      padding: 0 .1rem;
+  span {
+    // margin-left: 0.2rem;
+    font-size: .14rem;
+    font-weight: normal;
+    font-stretch: normal;
+    line-height: .07rem;
+    letter-spacing: 0px;
+    color: #888888;
+  }
+  .hang {
+    margin-left: 0.05rem;
+    font-family: PingFang-SC-Medium;
+    font-size: .14rem;
+    font-weight: normal;
+    letter-spacing: 0px;
+    color: #b0b0b0;
+  }
+  }
+}
+</style>
--- src/views/student/sanxiaosheng.vue
+++ src/views/student/sanxiaosheng.vue
@@ -1,11 +1,19 @@
 <template>
-    <div>
-        三校生
-    </div>
+  <div>
+    <Three />
+    <Pages/>
+    <Threes/>
+  </div>
 </template>
-
 <script>
+import Three from "../Thschool/school_lun.vue";
+import Pages from "../Thschool/page.vue";
+import Threes from "../Thschool/school-text.vue";
 export default {
-   
-}
+  components: {
+    Three,
+    Pages,
+    Threes
+  },
+};
 </script>
